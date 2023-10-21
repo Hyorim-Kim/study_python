219241c496f4b3bcffaf1030e37609f11e9ad94a
Hyorim-Kim <hyo1710@naver.com>
2023-10-21 22:23:48 +0900

    Django practice

--- djpro9ex/pro9app/views.py
+++ djpro9ex/pro9app/views.py
@@ -6,7 +6,8 @@ def Main(request):
     return render(request, 'index.html')
 
 def DbShow(request):
-    datas = Article.objects.all()
+    datas = Article.objects.all()  # DB에서 모든 Article 객체를 가져와 datas 변수에 할당
     print(datas)
     print(datas[0].irum)
     return render(request, 'list.html', {'datas':datas})
+    # list.html 템플릿에 datas를 전달하여 렌더링한 결과를 클라이언트에 반환
