bc38083549ff9e61bc1c329664c4a4313ff361df
Hyorim-Kim <hyo1710@naver.com>
2023-10-29 05:50:29 +0900

    django example

---
+++ djpro18ajaxex2/pro18app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Producttab(models.Model):
+    category = models.CharField(max_length=20)
+    pname = models.CharField(max_length=20)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    
+    
+    def __str__(self):
+        return self.title
