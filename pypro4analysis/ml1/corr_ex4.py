77faff52d591d3a66684b0bc9e58a42c9a1a98a4
Hyorim-Kim <hyo1710@naver.com>
2023-11-14 00:24:09 +0900

    2023/11/14 correlation review

--- pypro4analysis/ml1/corr_ex4.py
+++ pypro4analysis/ml1/corr_ex4.py
@@ -1,27 +1,24 @@
 # Advertising.csv 파일을 읽어 tv,radio,newspaper 간의 상관관계를 파악하시오. 
 # 그리고 이들의 관계를 heatmap 그래프로 표현하시오. 
-# 파일질라 참고 25라인부터
 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 plt.rc('font', family='malgun gothic')
 
-
 data = pd.read_csv('../testdata/Advertising.csv')
 print(data.head(3), data.shape)
 
-df1 = pd.DataFrame(data, columns=('tv','radio','newspaper'))
-print(df1.head(3))
+df = pd.DataFrame(data, columns=('tv','radio','newspaper'))
+print(df.head(3))
 
-print()
-# 상관계수
-print(df1.corr(method='pearson'))
+print('\n상관계수')
+print(df.corr(method='pearson'))
 # tv, radio : 0.054809
 # tv, newspaper : 0.056648
 # radio, newspaper : 0.354104
 
-# heatmap
+print('\nheatmap')
 import seaborn as sns
-sns.heatmap(df1.corr())
+sns.heatmap(df.corr())
 plt.show()
