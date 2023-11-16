c13c089c474d0c8ebe4274c55c3d3f69cfa8bf94
Hyorim-Kim <hyo1710@naver.com>
2023-11-16 17:35:33 +0900

    2023/11/16 logistic regression

---
+++ pypro4analysis/ml2/cla5_ex.py
@@ -0,0 +1,27 @@
+# [로지스틱 분류분석 문제3] 'testdata/advertisement.csv' 
+# 참여 칼럼 : 
+#   Daily Time Spent on Site : 사이트 이용 시간 (분)
+#   Age : 나이,
+#   Area Income : 지역 소독,
+#   Daily Internet Usage:일별 인터넷 사용량(분),
+#   Clicked Ad : 광고 클릭 여부 ( 0 : 클릭x , 1 : 클릭o )
+# 광고를 클릭('Clicked on Ad')할 가능성이 높은 사용자 분류.
+# 데이터 간의 단위가 큰 경우 표준화 작업을 시도한다.
+# ROC 커브와 AUC 출력
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
